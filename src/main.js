06c5c5969bafdd4c132e7ded14b4dd31f376f6e6
Luiz Rosane <lphilippirosane@gmail.com>
2024-01-10 22:59:24 +0100

    #1 Install and setup bootstrap

--- src/main.js
+++ src/main.js
@@ -7,4 +7,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import 'bootstrap/dist/css/bootstrap.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.js';
+
+
+createApp(App).use(bootstrap).mount('#app')
+
+
+
+
