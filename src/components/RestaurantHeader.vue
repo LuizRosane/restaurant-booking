60fc4eac0c3d09ab73d75142768b47e45aa704a2
Luiz Rosane <lphilippirosane@gmail.com>
2024-01-11 04:38:14 +0100

    #1 Restructure Restaurant header component

--- src/components/RestaurantHeader.vue
+++ src/components/RestaurantHeader.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="restaurant-container text-center">
-      <h1 class="restaurant-name">My Imaginary Bistro</h1>
+    <div class="restaurant-container">
+      <div>
+        <h1 class="restaurant-name">My Imaginary Bistro</h1>
+      </div>      
     </div>
+    <h2 class="cheff-name" style="text-align: center;">By Luiz Rosane</h2>
   </template>
   
   <style scoped>
@@ -16,9 +19,18 @@
   }
   
   .restaurant-name {
-    font-family: 'Your Contemporary Font', sans-serif; /* Replace with your chosen font */
-    font-size: 3rem; /* Adjust as needed */
+    font-family:'Courier New', Courier, monospace; 
+    font-size: 2.5rem; 
+  }
   
+  .cheff-name {
+    font-family:'Courier New', Courier, monospace; 
+    font-size: 1.5rem; 
+    color: gray;
+    padding-bottom:3rem !important;
+    width: 80vw;
+    
+    border-bottom: 0.1rem solid;
   }
   </style>
   
