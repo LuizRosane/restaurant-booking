4eb18b8d8d393d07f0ffc5adbe6d1c9d25722719
Luiz Rosane <lphilippirosane@gmail.com>
2024-01-12 02:25:19 +0100

    #1 Create staff login place holder view

---
+++ src/views/StaffLogin.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p>This is the login placeholder!!!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'StaffLogin',
+  };
+
+  </script>
+  
