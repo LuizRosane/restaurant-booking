f24ecd41f1baa1fd4ddec167c16c99595842bfe0
Luiz Rosane <lphilippirosane@gmail.com>
2024-01-12 02:43:11 +0100

    #1 Setup routes

--- src/main.js
+++ src/main.js
@@ -11,8 +11,23 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import bootstrap from 'bootstrap/dist/js/bootstrap.js';
 
+import { createRouter, createWebHistory } from 'vue-router';
 
-createApp(App).use(bootstrap).mount('#app')
+import Home  from "@/views/Home.vue"
+import StaffLogin  from "@/views/StaffLogin.vue"
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Home', component: Home},
+        {path: '/staff-login', name: 'Staff-Login', component: StaffLogin}
+    ]
+})
+
+const app = createApp(App)
+app.use(bootstrap)
+app.use(router)
+app.mount('#app')
 
 
 
